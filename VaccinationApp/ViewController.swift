4d07dc2e3255f4203bf4460b8e5a4073d7c10373
maryam <maryam_moein@hotmail.com>
2025-07-03 23:48:41 +0330

    Using our viewModel in viewController

--- VaccinationApp/ViewController.swift
+++ VaccinationApp/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    private let viewModel: VaccinesListViewModel
+    
+    init(viewModel: VaccinesListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.viewModel = VaccinesListViewModel()
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
