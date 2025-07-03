ba3a13b471f2bbf24b7b0773e2b11b76a2451779
maryam <maryam_moein@hotmail.com>
2025-07-04 00:19:11 +0330

    Add scrollView and contentView to the screen

--- VaccinationApp/Views/DetailViewController.swift
+++ VaccinationApp/Views/DetailViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
     private let vaccine: Vaccine
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
     
     init(vaccine: Vaccine) {
         self.vaccine = vaccine
@@ -22,5 +24,27 @@ class DetailViewController: UIViewController {
     override func loadView() {
         view = UIView()
         view.backgroundColor = Constants.Colors.background
+        setupScrollView()
+    }
+    
+    private func setupScrollView() {
+      scrollView.translatesAutoresizingMaskIntoConstraints = false
+      contentView.translatesAutoresizingMaskIntoConstraints = false
+
+      view.addSubview(scrollView)
+      scrollView.addSubview(contentView)
+
+      NSLayoutConstraint.activate([
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+        contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+        contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+        contentView.leadingAnchor.constraint(equalTo: scrollView.frameLayoutGuide.leadingAnchor),
+        contentView.trailingAnchor.constraint(equalTo: scrollView.frameLayoutGuide.trailingAnchor),
+        contentView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+      ])
     }
 }
