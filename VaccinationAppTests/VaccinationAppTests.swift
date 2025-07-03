08e59da96602e06f52d800a880fc853d6073965e
maryam <maryam_moein@hotmail.com>
2025-07-03 23:31:10 +0330

    Initial Commit

---
+++ VaccinationAppTests/VaccinationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  VaccinationAppTests.swift
+//  VaccinationAppTests
+//
+//  Created by Maryam on 7/3/25.
+//
+
+import Testing
+@testable import VaccinationApp
+
+struct VaccinationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
